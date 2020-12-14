7b08544ac26fe9e6eeabe02e7f417edfb4ce73de
Mncedisi <39966395+Mncedy@users.noreply.github.com>
2020-12-14 07:18:21 +0200

    Update task6.py

--- Desktop/LEVEL o CODING/task6.py
+++ Desktop/LEVEL o CODING/task6.py
@@ -1,15 +1,10 @@
-def max():
-    largest = num1
+def maxValue(num1, num2, num3):
+    max = num1
+    if num2 > max:
+        max = num2
+    if num3 > max:
+        max = num3
+    return max
 
-    if (num2 > largest):
-        largest = num2
-    if (num3 > largest):
-        largest = num3
-    print(largest)
 
-
-num1 = int(input("Enter a 1st number : "))
-num2 = int(input("Enter a 2nd number : "))
-num3 = int(input("Enter a 3rd number : "))
-
-max()
+print(maxValue(num1, num2, num3))
